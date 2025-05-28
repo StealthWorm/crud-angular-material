7cdad400422e30d93796ff4ee465ece9144d1584
StealthWorm <thierrypitela@hotmail.com>
2025-05-27 21:47:09 -0300

    refactor: included card container to display clients. Included logic to register list of clients into local storage

--- src/app/consult/consult.component.ts
+++ src/app/consult/consult.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { ClientService } from '../services/clients/client.service';
+import { Client } from '../register/client';
 
 @Component({
   selector: 'app-consult',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatTableModule,
+    MatCardModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './consult.component.html',
   styleUrl: './consult.component.scss'
 })
 export class ConsultComponent {
+  displayedColumns: string[] = ['id', 'name', 'email', 'phone', 'address', 'actions'];
+  clientsList: Client[] = [];
 
+  constructor(private clientService: ClientService) { }
+
+  // função do ciclo de vida do angular, similar ao componentDidMount do react
+  ngOnInit() {
+    this.clientsList = this.clientService.getAll();
+  }
+
+  searchClient(name: string) {
+    this.clientsList = this.clientService.getByName(name);
+  }
 }
--- src/app/services/clients/client.service.ts
+++ src/app/services/clients/client.service.ts
@@ -2,14 +2,35 @@ import { Injectable } from '@angular/core';
 import { Client } from '../../register/client';
 
 @Injectable({
-  // Informa que o serviço é um serviço de nível raiz, porem podemos atrelar a um módulo individualmente
+  // Informa que o serviço é de nível raiz, porem podemos atrelar a um módulo individualmente
   providedIn: 'root'
 })
 export class ClientService {
+  static REPO_CLIENTS: "_CLIENTS";
 
   constructor() { }
 
   register(client: Client) {
-    console.log('register ', client);
+    const clients = this.getAll();
+    clients.push(client);
+
+    localStorage.setItem(ClientService.REPO_CLIENTS, JSON.stringify(clients));
+  }
+
+  getAll(): Client[] {
+    const clients = localStorage.getItem(ClientService.REPO_CLIENTS);
+
+    if (clients) {
+      const clientsArray: Client[] = JSON.parse(clients);
+      return clientsArray;
+    }
+
+    localStorage.setItem(ClientService.REPO_CLIENTS, JSON.stringify([]));
+    return [];
+  }
+
+  getByName(name: string): Client[] {
+    const clients = this.getAll();
+    return clients.filter((client) => client?.name?.toLowerCase().includes(name.toLowerCase()));
   }
 }
